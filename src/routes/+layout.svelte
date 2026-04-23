56bf3af1f2be1e4a469cb86ef948cfda0a23cce4
yang siyu <164805339+yangsiyu616@users.noreply.github.com>
2026-04-23 16:02:09 +0200

    add custom fonts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,6 +8,19 @@ import "$lib/styles/reset.css"
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+
+	<!-- satoshi font -->
+	<link rel="preconnect" href="https://api.fontshare.com/"/>
+	<link rel="preconnect" href="https://api.fontshare.com/" crossorigin>
+	<link href="https://api.fontshare.com/v2/css?f[]=satoshi@1,2&display=swap" rel="stylesheet">
+
+		<!-- inter font -->
+<link rel="preconnect" href="https://fonts.bunny.net/">
+<link rel="preconnect" href="https://fonts.bunny.net/" crossorigin>
+<link href="https://fonts.bunny.net/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
+
+<link rel="preconnect" href="https://rsms.me/">
+<link rel="stylesheet" href="https://rsms.me/inter/inter.css">
 </svelte:head>
 
 {@render children()}
