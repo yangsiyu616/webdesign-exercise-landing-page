d4024596f7bd34e092f703d695a5eca801a5cedb
yang siyu <164805339+yangsiyu616@users.noreply.github.com>
2026-04-23 15:33:40 +0200

    configure mdsvex

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,11 @@
 import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { mdsvex } from 'mdsvex';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	extensions:['.svelte','.md'],
+	preprocess:[vitePreprocess(),mdsvex({extensions:['.md']})],
 	compilerOptions: {
 		// Force runes mode for the project, except for libraries. Can be removed in svelte 6.
 		runes: ({ filename }) => (filename.split(/[/\\]/).includes('node_modules') ? undefined : true)
